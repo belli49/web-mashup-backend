8228546b0ee576c55395bbce6c408b63ce3d3325
belli49 <pedrokomessu@gmail.com>
2023-07-11 04:02:33 +0900

    added basic API support

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
 
-console.log('test')
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+const app = express();
+app.use(express.json());
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const port = process.env.PORT || 5000;
+
+// REQUESTS
+
+app.post("/ask", async (req, res) => {
+  const prompt = req.body.prompt;
+  try {
+    if (prompt == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "system", "content": prompt}],
+    });
+
+    const completion = response.data.choices[0].message;
+    return res.status(200).json({
+      success: true,
+      message: completion,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}!!`));
